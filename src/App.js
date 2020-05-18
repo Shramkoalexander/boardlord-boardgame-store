2c970698da421a8f6a7f2baf129c927b52a31dc4
Alexander <shramkoalexander@gmail.com>
2020-05-18 20:47:26 +0500

    add collection data and collection state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.scss";
 import Header from "./components/header/header.component";
+import { COLLECTION_DATA } from "./components/collection/collection.data";
 
 function App() {
+  const initialState = COLLECTION_DATA;
+  const [collection] = useState(initialState.collection);
+  const [currency] = useState(initialState.currency);
+
   return (
     <div className="App">
       <Header />
