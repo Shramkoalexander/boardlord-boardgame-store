254158e89b57709c74b7ea4ba38a756e84151d6d
Alexander <shramkoalexander@gmail.com>
2020-11-02 14:55:43 +0500

    add collection-list component

---
+++ src/components/collection-list/collection-list.component.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import CollectionItem from "../collection-item/collection-item.component";
+import { connect } from "react-redux";
+import { selectCurrency } from "../../redux/shop/shop.selectors";
+import { createStructuredSelector } from "reselect";
+import withSpinner from "../spinner/withSpinner.component";
+import uniqid from "uniqid";
+import EmptyBlock from "../empty-block/empty-block.component";
+
+function CollectionList({ collection, currency }) {
+  return (
+    <>
+      {collection.length > 0 ? (
+        <div className="row no-gutters row-cols-1 row-cols-md-2 row-cols-lg-3 mx-n3">
+          {collection.map((item) => (
+            <div className="col p-3" key={uniqid()}>
+              <CollectionItem item={item} currency={currency} />
+            </div>
+          ))}
+        </div>
+      ) : (
+        <EmptyBlock>Ничего не найдено</EmptyBlock>
+      )}
+    </>
+  );
+}
+
+const mapStateToProps = createStructuredSelector({
+  currency: selectCurrency,
+});
+
+export default connect(mapStateToProps)(withSpinner(CollectionList));
