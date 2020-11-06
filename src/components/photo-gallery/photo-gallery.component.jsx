ba288929a2bab7165a71894d200f65f7f6182843
Alexander <shramkoalexander@gmail.com>
2020-11-06 23:02:43 +0500

    add photo-gallery component

---
+++ src/components/photo-gallery/photo-gallery.component.jsx
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from "react";
+import uniqid from "uniqid";
+import styles from "./photo-gallery.module.scss";
+
+import LightBox from "../light-box/light-box.component";
+import { connect } from "react-redux";
+import {
+  showLightBox,
+  setCurrentImageIndex,
+} from "../../redux/light-box/light-box.actions";
+
+function PhotoGallery({ imageName, showLightBox, setCurrentImageIndex }) {
+  const imagesCount = 4;
+
+  const [imageURLs, setImageURLs] = useState([]);
+
+  const handleOnClick = (e) => {
+    showLightBox();
+    setCurrentImageIndex(parseInt(e.currentTarget.dataset.id));
+  };
+
+  useEffect(() => {
+    try {
+      const URLsToAdd = [];
+      const url = require(`../../assets/images/boardgames/${imageName}/${imageName}.jpg`);
+      URLsToAdd.push(url);
+      for (let i = 1; i < imagesCount; i++) {
+        try {
+          const url = require(`../../assets/images/boardgames/${imageName}/${imageName}_${i}.jpg`);
+          URLsToAdd.push(url);
+        } catch (error) {
+          console.error(error.message);
+          continue;
+        }
+      }
+
+      setImageURLs(URLsToAdd);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }, [imageName]);
+
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.mainImageWrapper}>
+          <img
+            src={imageURLs[0]}
+            alt="board game"
+            onClick={handleOnClick}
+            data-id={0}
+            className={styles.mainImage}
+          />
+        </div>
+
+        <div className={styles.extraImagesContainer}>
+          {imageURLs.slice(1).map((url, index) => (
+            <div
+              key={uniqid()}
+              onClick={handleOnClick}
+              data-id={index + 1}
+              className={styles.extraImage}
+              style={{
+                backgroundImage: `url(${url})`,
+              }}
+            ></div>
+          ))}
+        </div>
+      </div>
+      {<LightBox imageURLs={imageURLs} />}
+    </>
+  );
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  showLightBox: () => dispatch(showLightBox()),
+  setCurrentImageIndex: (newIndex) => dispatch(setCurrentImageIndex(newIndex)),
+});
+
+export default connect(null, mapDispatchToProps)(PhotoGallery);
