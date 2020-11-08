6060429ad7508c3616d2663d969908a9604a97e0
Alexander <shramkoalexander@gmail.com>
2020-11-08 16:52:39 +0500

    edit Index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.scss";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import "./globals/styles/index.scss";
+import App from "./components/app/app";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import { store } from "./redux/store";
+import BreakpointsProviderBootstrapContainer from "./components/breakpoints-provider/breakpoints-provider-bootstrap.container";
+import PersistManager from "./components/persist-manager/persist-manager.component";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <PersistManager>
+          <BreakpointsProviderBootstrapContainer>
+            <App />
+          </BreakpointsProviderBootstrapContainer>
+        </PersistManager>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
