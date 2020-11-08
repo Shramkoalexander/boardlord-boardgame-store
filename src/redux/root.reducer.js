e4d4512558a60797ca6c816161dcd96881c21bf8
Alexander <shramkoalexander@gmail.com>
2020-11-08 16:45:43 +0500

    edit root reduxer

--- src/redux/root.reducer.js
+++ src/redux/root.reducer.js
@@ -4,10 +4,39 @@ import paginationReducer from "./pagination/pagination.reducer";
 import shopReducer from "./shop/shop.reducer";
 import sortingReducer from "./sorting/sorting.reducer";
 import searchReducer from "./search/search.reducer";
-export default combineReducers({
+import cartReducer from "./cart/cart.reducer";
+import sidebarMenuReducer from "./sidebar-menu/sidebar-menu.reducer";
+import breakpointsProviderReducer from "./breakpoints-provider/breakpoints-provider.reducer";
+import fixedHeaderWrapperReducer from "./fixed-header-wrapper/fixed-header-wrapper.reducer";
+import lightBoxReducer from "./light-box/light-box.reducer";
+import favoritesReducer from "./favorites/favorites.reducer";
+import userReducer from "./user/user.reducer";
+import directoriesReducer from "./directories/directories.reducer";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { cartTransform, favoritesTransform } from "./redux-persist.utils";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart", "favorites"],
+  transforms: [cartTransform, favoritesTransform],
+};
+
+const rootReducer = combineReducers({
   filter: filterReducer,
   pagination: paginationReducer,
   shop: shopReducer,
   sorting: sortingReducer,
   search: searchReducer,
+  directories: directoriesReducer,
+  cart: cartReducer,
+  sidebarMenu: sidebarMenuReducer,
+  breakpointsProvider: breakpointsProviderReducer,
+  fixedHeaderWrapper: fixedHeaderWrapperReducer,
+  lightBox: lightBoxReducer,
+  favorites: favoritesReducer,
+  user: userReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
